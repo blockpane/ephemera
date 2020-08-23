baf7e000f1474b522e0ba072f38702973ec40e67
fmalykh <49435326+fmalykh@users.noreply.github.com>
2020-08-23 20:24:24 +0300

    Add files via upload

---
+++ terraform/cloudflare/vars.tf
@@ -0,0 +1,15 @@
+# export TF_VAR_CLOUDFLARE_API_TOKEN=
+variable "CLOUDFLARE_API_TOKEN" {
+  description = "Cloudflare API token value"
+}
+
+# export TF_VAR_CLOUDFLARE_ACCOUNT_ID=
+variable "CLOUDFLARE_ACCOUNT_ID" {
+  description = "Cloudflare Account ID"
+}
+
+# export TF_VAR_CLOUDFLARE_ACCOUNT_ID=
+variable "CLOUDFLARE_ZONE_ID" {
+  description = "Cloudflare Zone ID to create record for ephemera"
+}
+
